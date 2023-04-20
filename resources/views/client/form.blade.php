53c07a2d1f4dab411278c1a9486a9979fb039cc0
Mendozape <erasto.mendoza.perez@gmail.com>
2023-04-20 14:00:39 -0600

    URL image form fixed

--- resources/views/client/form.blade.php
+++ resources/views/client/form.blade.php
@@ -19,7 +19,6 @@
     <div class="mb-3">
         <label for="name" class="form-label">Nombre</label>
         <input type="text" name="name" class="form-control" placeholder="Nombre del cliente"  value="{{old('name') ?? @$client->name }}">
-        <p class="form-text">Escriba el nombre el cliente </p>
         @error('name')
             <p class="form-text text-danger">{{$message}}</p>
         @enderror
@@ -27,20 +26,23 @@
     <div class="mb-3">
         <label for="due" class="form-label">Saldo</label>
         <input type="text" name="due" class="form-control" placeholder="Saldo del cliente" step="0.01" value="{{ old('due') ?? @$client->due }}">
-        <p class="form-text">Escriba saldo del cliente </p>
         @error('due')
             <p class="form-text text-danger">{{$message}}</p>
         @enderror
     </div>
+    
+    @if(isset($client))
     <div class="input-group mb-3">
-        <img src="{{ asset('storage/images/products/'.$client->image) }}" />    
-        <label for="ImageControl"/>Seleccione imagen</label>
+        <img height="50px" src="{{ asset('storage/images/products/'.$client->image) }}" />
+    </div>
+    @endif
+    <div class="input-group mb-3">
+        <label  for="ImageControl"/>Seleccione imagen</label>
         <input type="file" class="form-control-file" name=" image" id="inputGroupFile03" aria-describedby="inputGroupFileAddon03" aria-label="Upload">
     </div>
     <div class="mb-3">
         <label for="comments" class="form-label">Comentarios</label>
         <textarea name="comments" id="comments" cols="30" rows="10" class="form-control" placeholder="Escriba un comentario" >{{old('comments') ?? @$client->comments}}</textarea>
-        <p class="form-text">Escriba escriba un comentario </p>
         @error('commments')
             <p class="form-text text-danger">{{$message}}</p>
         @enderror
