8322f44f1e4047a9e13cf8ea27234fb5f71627b2
Mendozape <erasto.mendoza.perez@gmail.com>
2023-10-17 13:28:57 -0600

    apiss

--- routes/api.php
+++ routes/api.php
@@ -18,8 +18,6 @@ use Illuminate\Support\Facades\Route;
 /*Route::prefix('V1')->group(function(){
     Route::apiResource('/articles',ArticleController::class);
 });*/
-
-
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
