cff6aafacd94c729741beb4505670a884ba7a14b
Mendozape <erasto.mendoza.perez@gmail.com>
2023-11-13 15:01:03 -0600

    api testing

--- routes/api.php
+++ routes/api.php
@@ -21,8 +21,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
+Route::middleware(['auth'])->group(function(){
+    Route::get('/users',[ApiController::class,'users']);
+});
 //public API
-Route::get('/users',[ApiController::class,'users']);
+
 Route::post('/login',[ApiController::class,'login']);
 Route::apiResource('/articles',ArticleController::class);
