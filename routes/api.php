28b2b086067a2d7f923c3b6a0f0dd59c3af32072
Mendozape <erasto.mendoza.perez@gmail.com>
2023-11-21 14:58:46 -0600

    api basic auth

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,6 @@ use App\Http\Controllers\ApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
