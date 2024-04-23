db0c5d75e2e426f0fd239120147184792eb48223
Mendozape <erasto.mendoza.perez@gmail.com>
2024-04-23 14:55:50 -0600

    rect router

--- resources/js/Notification.jsx
+++ resources/js/Notification.jsx
@@ -1,69 +1,8 @@
-//import React from 'react';
-import React, { useState, useEffect } from 'react';
-import { createRoot } from 'react-dom/client'
-import axios from 'axios';
-const Notifications = () => {
-    const [isAdmin, setIsAdmin] = useState('');
-    
-    
-    let notis = [ ];
-    let notis2 = '';
-    let notis3 = '';
-    useEffect(() => {
-        axios.get('/api/admin/isAdmin')
-            .then(response => {
-                setIsAdmin(response.data.admin);
-                //setNotis(response.data);
-            })
-            .catch(error => {
-                console.error('Error fetching admin: ', error);
-            });
-    }, []);
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './example';
 
-    if(isAdmin.length>=1){
-        for (let Key in isAdmin) {
-            notis.push(isAdmin[Key]);
-        }
-
-        notis = notis.map(row => (
-            <>
-                <li key={row.id}>[ { row.updated_at } ] User { row.data.name } has just registered.
-                    <a href="route('route('NotiUpdate',4b4aaf53-2b65-48ac-84ac-9be8c181f25f)') " class='float-right mark-as-read' data-id='{{ row.id }}'> Mark as read</a>
-                </li>
-            </>
-        ));
-    }else{
-        notis='no records found';
-    }
-
-    return (
-        <>
-            <section className="section">
-                <div className="section-header" align="center">
-                    <h1>Notifications</h1>
-                </div>
-                <div className="section-body mt-2">
-                    <div className="row">
-                        <div className="col-lg-12">
-                            <div className="card">
-                                <div className="card-body" >
-                                {
-                                notis
-                                }
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-        </>
-
-    );
-};
-export default Notifications;
-//}
-if (document.getElementById('notifications')) {
-    createRoot(document.getElementById('notifications')).render(<Notifications />)
-}
-
-
+ReactDOM.render(
+<App />,
+document.getElementById('notifications')
+);
