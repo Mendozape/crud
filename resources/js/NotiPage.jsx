3f815045e8fa9fc39ba54d1c09332cafd6dc9685
Mendozape <erasto.mendoza.perez@gmail.com>
2024-05-06 14:53:10 -0600

    router

--- resources/js/NotiPage.jsx
+++ resources/js/NotiPage.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { createRoot } from 'react-dom/client';
 import axios from 'axios';
 import { NavLink, Outlet } from "react-router-dom";
-//import './bootstrap';
+//import './bootstrap.js';
 
 export default function Notifications() {
     const [isAdmin, setIsAdmin] = useState('');
@@ -25,16 +25,17 @@ export default function Notifications() {
     }
     //let notis = [1,2,3,4,5,6];
     return (
-        <div className='flex gap-2'>
-            <div className='flex flex-col gap-2'>
-                <h1>Notifications</h1>
+        <div className='d-flex p-2'>
+            <h1>Notifications</h1>
+            <div className='d-inline-flex p-2 bd-highlight'>
+                
                 <ul>
                     {
                         notis.map((row) => (
                             <li>
                                 <NavLink key={row.id} to={`/home/${row.id}`}
                                     className={({ isActive }) => {
-                                        return isActive ? '' : 'text-primary-700';
+                                        return isActive ? 'text-primary-700' : '';
                                     }}
                                 >
                                     {row.data.name}</NavLink>
